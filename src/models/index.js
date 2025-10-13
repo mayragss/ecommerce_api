4e6557c53bedda19e3a8a4b1f022cf12632c02a0
mayragsss <mayra.g.dias@hotmail.com>
2025-10-13 17:58:57 +0100

    add source -- fix BD

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,5 @@
 const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "mysql"
 });
